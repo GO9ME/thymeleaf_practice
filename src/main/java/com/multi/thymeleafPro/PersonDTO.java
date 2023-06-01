ed78cfab22cc0458e856ee0532d4c1926d3eefbc
'sgustjd2' <'shin1133@naver.com'>
2023-06-01 15:26:30 +0900

    thymeleaf연습

---
+++ src/main/java/com/multi/thymeleafPro/PersonDTO.java
@@ -0,0 +1,14 @@
+package com.multi.thymeleafPro;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDTO {
+	private String id;
+	private String name;
+	private int jumsu;
+}
